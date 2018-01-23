7a97d27d4033184043b0268f0821c80b89063a68
Sebastien Klasa <klasa@IS231023>
2018-01-23 11:29:55 +0100

    Test counter 2 fonctionnel

--- Arduino/test_counter2/test_counter2.ino.ino
+++ Arduino/test_counter2/test_counter2.ino.ino
@@ -8,8 +8,7 @@ const byte CLKEN=0, CLKDIS=1, WSTRG=2; // TC_CCRx bits
 const byte TCCLKS=0, CLKI=3, BURST=4, LDBSTOP=6, LDBDIS=7, ETRGEDG=8, ABETRG=10,
            CPCTRG=14, WAVE=15, LDRA=16, LDRB=18; // TC_CMRx bits
 
-void setup()
-{
+void setup() {
   Serial.begin(9600); // initilize serial port to 9600 baud
   while (!Serial) { } // wait for serial port to initialize
 
@@ -26,12 +25,14 @@ void setup()
   REG_TC0_CMR1=((3 << ETRGEDG) | // trigger on both edges of trigger input
                 (1 << ABETRG) | // TIOA is used as the external trigger
                 (1 << LDRA) | // load RA on rising edge of trigger input
-                (2 << LDRB)); // load RB on falling edge of trigger input
+                (2 << LDRB) |
+                (6 << TCCLKS)
+               ); // load RB on falling edge of trigger input
   REG_TC0_IER1 = 0; // disable TC1 interrupts
   REG_TC0_IER1 = ((1 << LDRAS) | (1 << LDRBS)); // enable TC1 RA and RB capture interrupts
   REG_TC0_IMR1 = ((1 << LDRAS) | (1 << LDRBS)); // enable TC1 capture interrupt mask
   REG_TC0_IDR1 = !((1 << LDRAS) | (1 << LDRBS)); // disable all other timer interrupts except capture
-  NVIC_EnableIRQ(TC1_IRQn); // enable TC1 interrupts
+  //NVIC_EnableIRQ(TC1_IRQn); // enable TC1 interrupts
 
   // print the startup header
   Serial.println("Timer Capture");
@@ -43,9 +44,8 @@ void setup()
   // a jumper needs to be installed between pin 7 (PWM output) and A7 (TC0,1 TIOA pin)
 }
 
-void loop()
-{
-  if (CaptureFlag) {
+void loop() {
+  /*if (CaptureFlag) {
     CaptureFlag=0;
     Serial.print(CaptureCountA);
     Serial.print(", ");
@@ -55,10 +55,18 @@ void loop()
   }
   else {
     Serial.println(REG_TC0_CV1);
-  }
+  }*/
+
+  Serial.print("REG_TC0_RA1 = ");
+  Serial.print(REG_TC0_RA1);
+  Serial.print(", REG_TC0_RB1 = ");
+  Serial.print(REG_TC0_RB1);
+  Serial.print(", REG_TC0_CV1 = ");
+  Serial.print(REG_TC0_CV1);
+  Serial.println("");
 }
 
-void TC1_Handler(){
+/*void TC1_Handler(){
   TimerCount=REG_TC0_CV1; // save the timer counter register, for testing
   TC_GetStatus(TC0, 1); // get status, TC0 channel 1, allow the interrupt to fire again
   if (MTIOA) {
@@ -68,4 +76,4 @@ void TC1_Handler(){
     CaptureCountB = REG_TC0_RB1; // get data from caputre register B for TC0 channel 1
   }
   CaptureFlag=1; // set flag idicating a new capture value present
-}
+}*/
